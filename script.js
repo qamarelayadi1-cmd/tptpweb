cafcca30229a2c5f349fa416556a2b2c3f7d17fb
qamarelayadi1-cmd <qamarelayadi1@gmail.com>
2026-05-16 16:18:35 +0100

    java

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
-console.log("Script loaded successfully!");
-const btn = document.getElementById("btn");
-const nom = document.getElementById("nom");
+const nom = document.getElementById("name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
- 
-
-console.log(btn);
-console.log(nom.value);
-console.log(email.value);
-console.log(password.value);
-
 const submit = document.getElementById("submit");
-if (nom.value === "" || email.value === "" || password.value === "") {
-    alert("Veuillez remplir tous les champs du formulaire.");
-} 
-if (!email.value.includes("@")) {
-    alert("Veuillez entrer une adresse email valide.");
-}
-if(password.value.length < 6) {
-    alert("Le mot de passe doit contenir au moins 6 caractères.");
-}
+
+console.log(nom);
+console.log(email);
+console.log(password);
+console.log(submit);
 
 submit.addEventListener("click", function(event) {
+    if (nom.value === "" || email.value === "" || password.value === "") {
+        alert("Veuillez remplir tous les champs.");
+    return;
+    }
+    if (password.value.length < 6) {
+        alert("Le mot de passe doit contenir au moins 6 caractères.");
+    return;
+    }
     event.preventDefault();
-    console.log("Button clicked!");
-    console.log("Nom: " + nom.value);
-    console.log("Email: " + email.value);
-    console.log("Password: " + password.value);
-    alert("bonne inscription");
-} );
-console.log("Password: " + password.value);
-alert("bonne inscription");
+    console.log(nom.value);
+    console.log(email.value);
+    console.log(password.value);
+    alert("Inscription réussie !");
+});
